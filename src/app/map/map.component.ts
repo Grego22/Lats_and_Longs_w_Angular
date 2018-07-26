4ed6f5a13c00aefbd66037ef52aa0e3d08a23c92
Greg Garville Jr <gjgarvil@gmail.com>
2018-07-26 15:58:36 -0400

    added firebase packages to app

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,6 @@
+import { GoogleMapsAPIWrapper } from '@agm/core/services/google-maps-api-wrapper';
 import { Component, OnInit } from '@angular/core';
+import {MapsAPILoader} from '@agm/core'
 
 @Component({
   selector: 'app-map',
